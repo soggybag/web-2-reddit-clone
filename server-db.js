59d2eeba0ad203b52ed7cdc5189e696874e54656
Mitchell <soggybag@gmail.com>
2018-10-18 20:04:30 -0700

    updated

--- server-db.js
+++ server-db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 /** Set up mongoose and connect to database */
 // --------------------------------------------------------
 mongoose.Promise = global.Promise;
-const connection = mongoose.connect('mongodb://localhost/redditclone', { useMongoClient: true });
+const connection = mongoose.connect('mongodb://localhost/redditclone', { useNewUrlParser: true });
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
 // mongoose.set('debug', true);
 
